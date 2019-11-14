35a630913ff82bf374e8bd889556f390e1bfd71b
atomsmasher81 <kartikgautam171@gmial.com>
2019-11-14 12:56:24 +0530

    added necessary models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from Person.models import PersonDetails
+
+
+class ChatInstance(models.Model):
+    person1 = models.ForeignKey(PersonDetails)
+    person2 = models.ForeignKey(PersonDetails)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class ChatMessage(models.Model):
+    instance = models.ForeignKey(ChatInstance,on_delete=models.PROTECT,)
+    sender = models.ForeignKey
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
